52b335c58e6788ee5a5bfcd73b944ec38e39b71a
Duong Dang <dddangmedia@gmail.com>
2019-10-10 08:44:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 require 'pry'
 
 def speak_to_grandma(phrase)
-  if phrase
+  if phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
